66560c9585547ef6d7d448e9ed026cabd8ab434c
marcopisani002 <pisanimarco34@gmail.com>
2023-01-12 10:32:12 +0100

    END NOW

--- universit.sql
+++ universit.sql
@@ -12,22 +12,31 @@ WHERE `cfu`>10;
 
 
 -- 3. Selezionare tutti gli studenti che hanno più di 30 anni
-SELECT * 
-FROM `students` 
-WHERE DATE(`date_of_birth`)<"1992-01-11";
+-- SELECT * 
+-- FROM `students` 
+-- WHERE DATE(`date_of_birth`)<"1992-01-11";
+
+SELECT *, TIMESTAMPDIFF(YEAR, `date_of_birth`, NOW())
+FROM `students`
+WHERE TIMESTAMPDIFF(YEAR, `date_of_birth`, NOW()) > 30;
 
 
 -- 4. Selezionare tutti i corsi del primo semestre del primo anno di un qualsiasi corso di
 -- laurea (286)
 SELECT *
  FROM `courses` 
- WHERE `period`="I semestre" 
- AND `year`=1;
+--  WHERE `period`="I semestre" 
+--  AND `year`=1;
+--NEL CASO NON CI SONO CONTROLLI ADEGUATI E L'UTENTE CAMBIA SCRITTURA(PIU FLESSIBILE)
+
+WHERE (`period` LIKE "I %"
+OR `period` LIKE "1 %")
+AND `year`=1;
 -- 5. Selezionare tutti gli appelli d'esame che avvengono nel pomeriggio (dopo le 14) del 20/06/2020 (21)
 SELECT * 
 FROM `exams`
- WHERE HOUR(`hour`)>"14:00:00" 
- AND DATE(`date`)="2020/06/20";
+ WHERE `hour`>"14:00:00"
+ AND DATE(`date`)="2020-06-20";
 
 -- 6. Selezionare tutti i corsi di laurea magistrale (38)
 SELECT * 
